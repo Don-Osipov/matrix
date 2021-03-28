b3d0a394c106336820008ada95b88d3902b06083
Don Osipov <donosipov@gmail.com>
2021-03-28 10:30:04 -0400

    update

--- main.py
+++ main.py
@@ -3,9 +3,28 @@ from draw import *
 from matrix import *
 
 screen = new_screen()
-color = [ 0, 255, 0 ]
+color = [255, 0, 0]
 matrix = new_matrix()
 
+for y in range(YRES + 1):
+    if y % 70 < 35:
+        add_edge(matrix, 0, y, 0, XRES // 2, y + (XRES // 2), 0)
+        add_edge(matrix, XRES // 2, y + (XRES // 2), 0, XRES, y, 0)
+draw_lines(matrix, screen, color)
 
-draw_lines( matrix, screen, color )
+color = [0, 0, 0]
+matrix2 = new_matrix()
+
+for x in range(XRES + 1):
+    if x < XRES // 2:
+        add_edge(matrix2, x, 0, 0, 0, YRES - x, 0)
+    else:
+        add_edge(matrix2, x, 0, 0, XRES, x, 0)
+
+draw_lines(matrix2, screen, color)
+
 display(screen)
+save_ppm(screen, "binary.ppm")
+save_ppm_ascii(screen, "ascii.ppm")
+save_extension(screen, "img.png")
+
--- matrix.py
+++ matrix.py
@@ -8,29 +8,67 @@ z0  z1  ... zn
 1  1        1
 """
 import math
+import copy as cp
 
-#print the matrix such that it looks like
-#the template in the top comment
-def print_matrix( matrix ):
-    pass
-
-#turn the paramter matrix into an identity matrix
-#you may assume matrix is square
-def ident( matrix ):
-    pass
-
-#multiply m1 by m2, modifying m2 to be the product
-#m1 * m2 -> m2
-def matrix_mult( m1, m2 ):
-    pass
+# print the matrix such that it looks like
+# the template in the top comment
+def print_matrix(matrix):
+    for row in matrix:
+        for el in row:
+            print(f"{el} ", end="")
+        print()
 
 
+# turn the paramter matrix into an identity matrix
+# you may assume matrix is square
+def ident(matrix):
+    for i, row in enumerate(matrix):
+        for j, el in enumerate(row):
+            if j == i:
+                matrix[i][j] = 1
+            else:
+                matrix[i][j] = 0
 
 
-def new_matrix(rows = 4, cols = 4):
+# multiply m1 by m2, modifying m2 to be the product
+# m1 * m2 -> m2
+def matrix_mult(m1, m2):
+
+    copy = cp.deepcopy(m2)
+
+    m1Row, m2Row = len(m1), len(copy)
+    m1Col, m2Col = len(m1[0]), len(copy[0])
+
+    if m1Col == m2Row:
+        m2 = new_matrix(m2Col, m1Row)
+
+        for i, row in enumerate(m1):
+            for j in range(m2Col):
+                colArr = getColumn(j, copy)
+                m2[i][j] = dotProduct(row, colArr)
+
+
+def dotProduct(arr1, arr2):
+    result = 0
+    for el1, el2 in zip(arr1, arr2):
+        result += el1 * el2
+    return result
+
+
+def getColumn(colNumber, arr):
+    if isinstance(arr[0], list):
+        col = []
+        for row in arr:
+            col.append(row[colNumber])
+        return col
+    return
+
+
+def new_matrix(rows=4, cols=4):
     m = []
-    for c in range( cols ):
-        m.append( [] )
-        for r in range( rows ):
-            m[c].append( 0 )
+    for c in range(cols):
+        m.append([])
+        for r in range(rows):
+            m[c].append(0)
     return m
+
